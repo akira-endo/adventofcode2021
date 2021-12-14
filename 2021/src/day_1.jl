c61a0141ed125f308a6c9a675300724cd24e5256
akira-endo <akira.endo@lshtm.ac.uk>
2021-12-14 18:10:35 +0000

    Initialise with AdventOfCode.jl

---
+++ 2021/src/day_1.jl
@@ -0,0 +1,14 @@
+# https://adventofcode.com/2021/day/1
+using AdventOfCode
+
+input = readlines("2021/data/day_1.txt")
+
+function part_1(input)
+    nothing
+end
+@info part_1(input)
+
+function part_2(input)
+    nothing
+end
+@info part_2(input)
