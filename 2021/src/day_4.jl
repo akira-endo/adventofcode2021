6c552ff6ff52c23117bcd7bee77cf881c7c2c937
akira-endo <akira.endo@lshtm.ac.uk>
2021-12-15 01:50:24 +0000

    refactor day 4

--- 2021/src/day_4.jl
+++ 2021/src/day_4.jl
@@ -9,29 +9,35 @@ end
 
 function mark(draw, board)
     markidx=findfirst(x->x==draw,board)
-    if isnothing(markidx) return(-1) end
-    board[markidx]=0
-    if iszero(@view board[markidx[1],:]) || iszero(@view board[:,markidx[2]]) # win
-        sum(board)*draw
-    else -1 end
+    if isnothing(markidx) return(0) end
+    board[markidx]=-1
+    if all(<(0),(@view board[markidx[1],:])) || all(<(0),(@view board[:,markidx[2]])) 1 # win
+        else 0 end
 end
 function part_1(input)
     boards=copy.(input.boards) # not to mutate the original input
-    res=nothing
+    res=zeros(size(boards))
+    score=0
     for draw in input.draws
-        res=mark.(draw,boards)
-        if any(>(-1),res) break end
+        res.+=mark.(draw,boards)
+        if any(>(0),res)
+            score=sum(max.(boards[findfirst(>(0),res)],0))*draw
+            break
+        end
     end
-    maximum(res)
+    score
 end
 
 function part_2(input)
     boards=copy.(input.boards) # not to mutate the original input
     res=zeros(size(boards))
+    score=0
     for draw in input.draws
-        res.+=mark.(draw,boards).+1
-        if all(>(0),res) break end
-        res[res.>0].=Inf
+        res.+=mark.(draw,boards)
+        if all(>(0),res)
+            score=sum(max.(boards[findmin(res)[2]],0))*draw
+            break
+        end
     end
-    Int(minimum(res)-1)
+    score
 end
